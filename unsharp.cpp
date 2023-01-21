b4ce2bb215fab1722fbacf5ac83154fa11698589
makotoyamaai <97353852+makotoyamaai@users.noreply.github.com>
2023-01-21 13:33:03 +0900

    Create unsharp.cpp

---
+++ unsharp.cpp
@@ -0,0 +1,80 @@
+/////////////////
+// unsharp.cpp //
+/////////////////
+
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+
+int main(int argc, const char* argv[])
+{
+	int hr = -1;
+	
+	try
+	{
+		Mat src, dst;
+		
+		///////////////////
+		// Load an image //
+		///////////////////
+		// As usual we load our source image ``src'':
+		const char* imageName = argc >= 2 ? argv[1] : "./images/cosmobio3.jpg";
+		src = imread(samples::findFile(imageName), IMREAD_COLOR); // Load an image
+		
+		///////////////////
+		// Create cernel //
+		///////////////////
+		// In this case, 3x3 mask
+		Mat kernel = (Mat_<float>(3, 3) <<
+		                   ////////////////////////////
+			0, -1, 0,  //           / 0, -1,  0\ //
+			-1, 6, -1, // cernel is |-1,  5, -1| //
+			0, -1, 0); //           \ 0, -1,  0/ //
+		                   ////////////////////////////
+		filter2D(
+			src,
+			dst,
+			-1,
+			kernel,
+			Point(-1, -1),
+			0,
+			BORDER_DEFAULT
+		);
+		
+		std::cout << "Successed" << std::endl;
+		
+		////////////////////////////
+		// Show src (input) image //
+		////////////////////////////
+		namedWindow("Source", 1);
+		imshow("Source", src);
+		
+		//////////////////////////
+		// Show unsharped image //
+		//////////////////////////
+		namedWindow("unsharp", 1);
+		imshow("unsharp", dst);
+		// save unsharped image
+		imwrite("./images/cosmobio3-unsharp.jpg", dst);
+		
+		waitKey(0);
+		
+		hr = 0;
+	}
+	
+	catch (Exception ex)
+	{
+		std::cout << ex.err << std::endl;
+	}
+	
+	/////////////////////////
+	// Destroy all windows //
+	/////////////////////////
+	destroyAllWindows();
+	
+	return hr;
+}
